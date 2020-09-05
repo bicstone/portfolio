4adbe3ea041caf08abc6b9062e8bdd85f768e577
Takanori Oishi <takanori0014@live.jp>
2020-09-05 17:55:09 +0900

    feat: 翻訳対応 #2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
+import { useI18next } from 'gatsby-plugin-react-i18next';
 
-const home: React.FC = () => <div>Hello world!</div>;
+const home: React.FC = () => {
+  const { t } = useI18next();
+  return <div>{t('Hello world!')}</div>;
+};
 
 export default home;
