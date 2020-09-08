aa9ddbeba7bdae5ebe4671769e0408820833c90a
Takanori Oishi <takanori0014@live.jp>
2020-09-08 23:42:18 +0900

    feat: Google analytics タグの埋め込み #29

--- .gatsby/gatsby-config.ts
+++ .gatsby/gatsby-config.ts
@@ -45,10 +45,11 @@ module.exports = {
       resolve: 'gatsby-plugin-root-import',
     },
     {
-      resolve: `gatsby-plugin-gdpr-cookies`,
+      resolve: 'gatsby-plugin-gdpr-cookies',
       options: {
         googleAnalytics: {
           trackingId: 'UA-165073691-2',
+          anonymize: false,
         },
         environments: ['production', 'development'],
       },
--- src/components/CookieAlert.tsx
+++ src/components/CookieAlert.tsx
@@ -3,34 +3,42 @@ import Cookies from 'js-cookie';
 import { Snackbar, Button, Typography } from '@material-ui/core';
 import { Link } from 'gatsby-theme-material-ui';
 
-const CookieAlert: React.FC = () => {
-  const CookieName = 'gatsby-gdpr-google-analytics';
-  const CookieTrue = 'true';
-  const CookieFalse = 'false';
-  const CookieOptions: Cookies.CookieAttributes = {
+interface Props {
+  cookieName: string;
+  cookieTrue: string;
+  cookieFalse: string;
+  cookieOptions: Cookies.CookieAttributes;
+}
+
+const CookieAlert: React.FC<Props> = ({
+  cookieName = 'gatsby-gdpr-google-analytics',
+  cookieTrue = 'true',
+  cookieFalse = 'false',
+  cookieOptions = {
     expires: 790,
     secure: process.env.NODE_ENV !== 'development',
     sameSite: 'Strict',
-  };
-  const [agree, setAgree] = React.useState(Cookies.get(CookieName));
+  },
+}) => {
+  const [agree, setAgree] = React.useState(Cookies.get(cookieName));
 
   const agreeAction = () => {
-    Cookies.set(CookieName, CookieTrue, CookieOptions);
-    setAgree(CookieTrue);
+    Cookies.set(cookieName, cookieTrue, cookieOptions);
+    setAgree(cookieTrue);
   };
 
   const closeAction = () => {
-    Cookies.set(CookieName, CookieFalse, CookieOptions);
-    setAgree(CookieFalse);
+    Cookies.set(cookieName, cookieFalse, cookieOptions);
+    setAgree(cookieFalse);
   };
 
   const agreeButton = (
     <>
-      <Button color="secondary" size="small" onClick={closeAction}>
-        拒否
-      </Button>
       <Button color="primary" onClick={agreeAction}>
         OK
+      </Button>
+      <Button color="secondary" size="small" onClick={closeAction}>
+        拒否
       </Button>
     </>
   );
