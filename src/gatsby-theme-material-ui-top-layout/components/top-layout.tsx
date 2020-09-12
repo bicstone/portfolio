c3cf4ed07504c27997401db3cc3878fea372ec80
Takanori Oishi <takanori0014@live.jp>
2020-09-12 18:18:13 +0900

    feat: ダークモード #47 (#51)

--- src/gatsby-theme-material-ui-top-layout/components/top-layout.tsx
+++ src/gatsby-theme-material-ui-top-layout/components/top-layout.tsx
@@ -3,12 +3,21 @@ import React from 'react';
 import { ThemeProvider, CssBaseline, createMuiTheme } from '@material-ui/core';
 import { jaJP } from '@material-ui/core/locale';
 
+import { themeReducer, themeInitialState, themeInitial } from '../../reducer/themeReducer';
+import { ThemeContext } from '../../hooks/use-darkmode';
+
 export const TopLayout: React.FC = props => {
+  const [themeState, themeDispatch] = React.useReducer(
+    themeReducer,
+    themeInitialState,
+    themeInitial,
+  );
+  const { darkMode } = themeState;
   // const defaultTheme = createMuiTheme();
   const theme = createMuiTheme(
     {
       palette: {
-        // type: darkMode ? 'dark' : 'light',
+        type: darkMode ? 'dark' : 'light',
         // primary: {
         //   main: '',
         // },
@@ -28,7 +37,7 @@ export const TopLayout: React.FC = props => {
     <>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        {props.children}
+        <ThemeContext.Provider value={themeDispatch}>{props.children}</ThemeContext.Provider>
       </ThemeProvider>
     </>
   );
---
+++ src/reducer/themeReducer.ts
@@ -0,0 +1,56 @@
+import React from 'react';
+const inBrowser = () => typeof window !== 'undefined';
+
+const LOCAL_STORAGE_NAME = 'paletteType';
+const LOCAL_STORAGE_TRUE = 'dark';
+const LOCAL_STORAGE_FALSE = 'light';
+export const TOGGLE_DARKMODE = 'TOGGLE_DARKMODE';
+
+export interface ThemeState {
+  darkMode: boolean;
+}
+export type ThemeAction = { type: typeof TOGGLE_DARKMODE };
+
+export const themeInitialState = {
+  darkMode: false,
+};
+
+/**
+ * テーマの状態
+ */
+export const themeReducer: React.Reducer<ThemeState, ThemeAction> = (state, action) => {
+  inBrowser() &&
+    window.localStorage.setItem(
+      LOCAL_STORAGE_NAME,
+      !state.darkMode ? LOCAL_STORAGE_TRUE : LOCAL_STORAGE_FALSE,
+    );
+  switch (action.type) {
+    case TOGGLE_DARKMODE:
+      return {
+        darkMode: !state.darkMode,
+      };
+    default:
+      return state;
+  }
+};
+
+/**
+ * テーマの状態・遅延初期化
+ * 設定順位
+ * 1. ローカルストレージの設定があれば引き継ぐ
+ * 2. OS設定でナイトモードならダークモード
+ * 3. ライトモード
+ */
+export const themeInitial = (): any => {
+  const localStorageDarkMode = inBrowser() && window.localStorage.getItem(LOCAL_STORAGE_NAME);
+  const prefersDarkMode = inBrowser() && window.matchMedia('(prefers-color-scheme: dark)').matches;
+  if (localStorageDarkMode === LOCAL_STORAGE_TRUE) {
+    return { darkMode: true };
+  } else if (localStorageDarkMode === LOCAL_STORAGE_FALSE) {
+    return { darkMode: false };
+  } else if (prefersDarkMode) {
+    return { darkMode: true };
+  } else {
+    return { darkMode: false };
+  }
+};
