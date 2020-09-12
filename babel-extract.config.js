ffe55e5895498bddb2d263f1927a06ae711a85c7
Takanori Oishi <takanori0014@live.jp>
2020-09-12 18:11:03 +0900

    feat: ダークモード Fixes #47

--- babel-extract.config.js
+++ babel-extract.config.js
@@ -8,7 +8,7 @@ module.exports = {
       'i18next-extract',
       {
         locales: languages.locales,
-        useI18nextDefaultValue: [languages.defaultLanguage],
+        useI18nextDefaultValue: true,
         // 整理する場合は外す(存在しない翻訳が消える)
         // discardOldKeys: true,
         outputPath: 'src/locales/{{locale}}/{{ns}}.json',
