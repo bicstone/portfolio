3d36f0bfd3ad8e0f026df8c0f78e7d0c8f6c8d26
Takanori Oishi <takanori0014@live.jp>
2020-09-18 03:28:45 +0900

    feat: 404ページ Fixes #38

---
+++ src/pages/404.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { PageProps } from 'gatsby';
+import { useI18next } from 'gatsby-plugin-react-i18next';
+
+import { Button } from 'gatsby-theme-material-ui';
+import { Box, Grid, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import LocalHotelIcon from '@material-ui/icons/LocalHotel';
+
+import Layout from '../components/Layout';
+
+const useStyles = makeStyles(theme => ({
+  icon: {
+    width: theme.spacing(20),
+    height: theme.spacing(20),
+  },
+}));
+
+const home: React.FC<PageProps> = ({ path }) => {
+  const classes = useStyles();
+  const { t } = useI18next();
+  return (
+    <Layout pathname={path} isHome>
+      <Grid container direction="column" justify="center" alignItems="center">
+        <Box textAlign="center">
+          <LocalHotelIcon className={classes.icon} />
+          <Typography variant="h4" component="h1" paragraph>
+            {t('not-found.title')}
+          </Typography>
+          <Typography variant="body1">{t('not-found.message')}</Typography>
+        </Box>
+        <Box marginTop={4} textAlign="center">
+          <Button variant="contained" to="/">
+            {t('not-found.back-to-home')}
+          </Button>
+        </Box>
+      </Grid>
+    </Layout>
+  );
+};
+
+export default home;
