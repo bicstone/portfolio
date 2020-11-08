a031b07a23ee07048962cfcb0ee9878779485cdb
Takanori Oishi <takanori0014@live.jp>
2020-11-09 02:00:21 +0900

    fix: プロジェクトをアコーディオンに変更 #106 (#107)

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
+import parse from 'html-react-parser';
 import { graphql, useStaticQuery } from 'gatsby';
-import Img from 'gatsby-image';
 import { useI18next } from 'gatsby-plugin-react-i18next';
 
 import {
@@ -8,67 +8,74 @@ import {
   Grid,
   Container,
   CardHeader,
-  CardContent,
-  Card,
   Avatar,
-  CardMedia,
-  Chip,
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  SvgIcon,
 } from '@material-ui/core/';
 import { makeStyles } from '@material-ui/core/styles';
-import DevicesIcon from '@material-ui/icons/Devices';
-import { ProjectDataQuery } from '../types/graphqlTypes';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { ProjectsDataQuery } from '../types/graphqlTypes';
+
+type Tags = Array<{ name?: string | null } | null | undefined> | null | undefined;
 
 const useStyles = makeStyles(theme => ({
   mainGrid: {
     marginTop: theme.spacing(3),
     marginBottom: theme.spacing(3),
   },
-  cardContent: {
-    paddingTop: 0,
-  },
-  cardMedia: {
-    height: theme.spacing(20),
+  cardHeader: {
+    padding: 0,
   },
 }));
 
 const Projects: React.FC = () => {
   const classes = useStyles();
   const { t, language } = useI18next();
-  const { allContentfulProject }: ProjectDataQuery = useStaticQuery(
+  const [expanded, setExpanded] = React.useState<string | false>(false);
+
+  const handleChange = (id: string) => (
+    event: React.ChangeEvent<Record<string, unknown>>,
+    isExpanded: boolean,
+  ) => {
+    setExpanded(isExpanded ? id : false);
+  };
+
+  const subHeader = (
+    expanded: boolean,
+    roles: Tags,
+    languages: Tags,
+    systems: Tags,
+    tools: Tags,
+    assigns: Tags,
+  ) =>
+    React.useMemo(() => {
+      const joinTags = (tags: Tags) => tags?.map(tag => tag?.name).join(' / ');
+
+      if (expanded) {
+        return `
+        ${joinTags(roles)} / ${joinTags(assigns)} / ${joinTags(languages)} / ${joinTags(
+          systems,
+        )} / ${joinTags(tools)}
+        `;
+      }
+      return `${joinTags(roles)} / ${joinTags(languages)} / ${joinTags(systems)}`;
+    }, [expanded, roles, languages, systems, tools]);
+
+  const { allContentfulProjects }: ProjectsDataQuery = useStaticQuery(
     graphql`
-      query ProjectData {
-        allContentfulProject {
+      query ProjectsData {
+        allContentfulProjects {
           edges {
             node {
               id
               node_locale
               name
-              slug
               startDate
               during
               comment
-              mainImage {
-                title
-                file {
-                  url
-                }
-                localFile {
-                  childImageSharp {
-                    fluid {
-                      sizes
-                      src
-                      srcSet
-                      aspectRatio
-                    }
-                  }
-                }
-              }
-              siteUrl
-              sourceUrl
               roles {
-                name
-              }
-              assigns {
                 name
               }
               systems {
@@ -79,6 +86,12 @@ const Projects: React.FC = () => {
               }
               tools {
                 name
+              }
+              assigns {
+                name
+              }
+              image {
+                image
               }
             }
           }
@@ -93,48 +106,51 @@ const Projects: React.FC = () => {
         {t('project.title')}
       </Typography>
       <Grid container spacing={2} className={classes.mainGrid} justify="center" alignItems="center">
-        {allContentfulProject.edges.map(
-          ({ node }) =>
-            node.node_locale === language && (
-              <Grid item xs={12} sm={6} md={4} key={node.id}>
-                <Card>
-                  <CardHeader
-                    avatar={
-                      <Avatar>
-                        <DevicesIcon />
-                      </Avatar>
-                    }
-                    title={
-                      <Typography component="h3" variant="h6">
-                        {node.name}
-                      </Typography>
-                    }
-                    subheader={node.roles ? node.roles.map(role => role?.name).join(' / ') : ''}
-                  />
-                  <CardMedia>
-                    {node?.mainImage?.localFile?.childImageSharp?.fluid && (
-                      <Img
-                        fluid={node.mainImage.localFile.childImageSharp.fluid}
-                        alt={node.mainImage.title ?? ''}
-                        className={classes.cardMedia}
-                      />
-                    )}
-                  </CardMedia>
-                  <CardContent>
-                    {node?.languages?.map(language => (
-                      <Chip label={language?.name} size="small" key={language?.name} />
-                    ))}
-                    {node?.systems?.map(system => (
-                      <Chip label={system?.name} size="small" key={system?.name} />
-                    ))}
-                  </CardContent>
-                  <CardContent className={classes.cardContent}>
+        <Grid item sm={12}>
+          {allContentfulProjects.edges.map(
+            ({ node }) =>
+              node.node_locale === language && (
+                <Accordion
+                  expanded={expanded === node.id}
+                  onChange={handleChange(node.id)}
+                  key={node.id}
+                >
+                  <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls={`${node.id}-content`}
+                    id={`${node.id}-header`}
+                  >
+                    <CardHeader
+                      className={classes.cardHeader}
+                      avatar={
+                        node?.image?.image && (
+                          <Avatar>
+                            <SvgIcon>{parse(node.image.image)}</SvgIcon>
+                          </Avatar>
+                        )
+                      }
+                      title={
+                        <Typography component="h3" variant="h6">
+                          {node.name}
+                        </Typography>
+                      }
+                      subheader={subHeader(
+                        expanded === node.id,
+                        node.roles,
+                        node.languages,
+                        node.systems,
+                        node.tools,
+                        node.assigns,
+                      )}
+                    />
+                  </AccordionSummary>
+                  <AccordionDetails>
                     <Typography variant="body2">{node.comment}</Typography>
-                  </CardContent>
-                </Card>
-              </Grid>
-            ),
-        )}
+                  </AccordionDetails>
+                </Accordion>
+              ),
+          )}
+        </Grid>
       </Grid>
     </Container>
   );
