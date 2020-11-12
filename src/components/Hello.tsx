97886f5b0afef7b89f713676b53a91adc468a238
Takanori Oishi <takanori0014@live.jp>
2020-11-13 00:52:48 +0900

    fix: typedを廃止 #113 (#114)

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import { useI18next } from 'gatsby-plugin-react-i18next';
-import Typed, { TypedOptions } from 'typed.js';
 
 import { Typography, Grid, Container } from '@material-ui/core/';
 import { makeStyles } from '@material-ui/core/styles';
@@ -17,25 +16,6 @@ const useStyles = makeStyles(theme => ({
 const Hello: React.FC = () => {
   const classes = useStyles();
   const { t } = useI18next();
-  const wrapEl = useRef<HTMLSpanElement>(null);
-  const stringsEl = useRef<HTMLUListElement>(null);
-  useEffect(() => {
-    if (stringsEl.current && wrapEl.current) {
-      const options: TypedOptions = {
-        backDelay: 2000,
-        typeSpeed: 40,
-        showCursor: false,
-        autoInsertCss: false,
-        fadeOut: true,
-        loop: true,
-        stringsElement: stringsEl.current,
-      };
-      const typed = new Typed(wrapEl.current, options);
-      return () => {
-        typed.destroy();
-      };
-    }
-  }, [stringsEl.current, wrapEl.current]);
 
   return (
     <Container maxWidth="md">
@@ -46,20 +26,11 @@ const Hello: React.FC = () => {
               <BicstoneIcon width={100} height={100} />
             </Grid>
             <Grid item xs={11} sm={10}>
-              <ul ref={stringsEl}>
-                <li>{t('hello.title.1')}</li>
-                <li>{t('hello.title.2')}</li>
-                <li>{t('hello.title.3')}</li>
-                <li>{t('hello.title.4')}</li>
-                <li>{t('hello.title.5')}</li>
-                <li>{t('hello.title.6')}</li>
-              </ul>
-              <Typography component="span" variant="body2">
-                {t('hello.title.pref')}
-                <span ref={wrapEl} />
+              <Typography component="span" variant="h6">
+                {t('hello.title')}
               </Typography>
               <Typography variant="body1" paragraph>
-                {t('hello-message')}
+                {t('hello.message')}
               </Typography>
             </Grid>
           </Grid>
