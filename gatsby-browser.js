4bfff49f67081dab9ec0ab7934e860625d5b2be5
Takanori Oishi <takanori0014@live.jp>
2020-11-29 23:15:47 +0900

    feat: CSPの追加 Fixes #126

---
+++ gatsby-browser.js
@@ -0,0 +1,10 @@
+/**
+ * サービスワーカーによる更新が完了したら強制リロード
+ * MEMO: このような実装は賛否両論あるが、ポートフォリオという
+ * 正確性が重要なコンテンツにおいて最新のコンテンツが見れるべきという判断をした（Slackとかはその実装）。
+ * 将来的には通知を表示して更新ボタンを押して貰うようにしたい（思ったより簡単にできそう）。
+ * SEE: https://www.gatsbyjs.com/docs/browser-apis/#onServiceWorkerUpdateFound
+ */
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export const onServiceWorkerUpdateReady = () => window.location.reload();
