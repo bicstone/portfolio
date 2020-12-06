7e1dadaa3d66ca481262d09bed59d85c261fd81e
Takanori Oishi <takanori0014@live.jp>
2020-12-07 03:08:05 +0900

    fix: manifestのLinkタグのURLを修正 Fixes #141

--- src/components/layouts/Head.tsx
+++ src/components/layouts/Head.tsx
@@ -37,7 +37,6 @@ export const Head: React.FC<Props> = ({
       <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-      <link rel="manifest" href="/site.webmanifest" />
       <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#2d1e1e" />
       <meta name="apple-mobile-web-app-title" content={defaultMetadata.shortTitle} />
       <meta name="application-name" content={defaultMetadata.shortTitle} />
