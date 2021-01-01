2047d31fb9aa5594c6162f3b7861e1af62f75a86
Takanori Oishi <takanori0014@live.jp>
2021-01-01 18:03:51 +0900

    chore(deps): update dependencies

--- src/components/icons/BicstoneIcon.tsx
+++ src/components/icons/BicstoneIcon.tsx
@@ -6,13 +6,8 @@ import parse, {
   DOMNode,
 } from 'html-react-parser';
 import { graphql, useStaticQuery } from 'gatsby';
+import { Element } from 'domhandler';
 import { BicstoneIconDataQuery } from '../../types';
-
-// HACK: html-react-parserが使っているdomhandlerはv3だが、
-// 他のライブラリの影響でnode_modulesではv4になっている。
-// こちらから読むとv4でinstanceofがtrueにならないため、直接参照する。
-// 時間があるときに、他にいい方法がないか調べる。
-import { Element } from '../../../node_modules/html-dom-parser/node_modules/domhandler/lib';
 
 type Props = {
   width: number;
