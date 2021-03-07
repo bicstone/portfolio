ad4f081154f988a3ec319202708293f925a7340a
Takanori Oishi <takanori0014@live.jp>
2021-03-07 10:39:26 +0900

    revert: "feat: ブログ #30 (#199)" (#200)
    This reverts commit 0d0f80fd8e451da8385a0e304c89e72b7bde687c.

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import { graphql, PageProps } from 'gatsby';
+import { PageProps } from 'gatsby';
 import { useI18next } from 'gatsby-plugin-react-i18next';
 import { Button } from 'gatsby-theme-material-ui';
-import { makeStyles, Container, Box, Typography } from '@material-ui/core';
+import { makeStyles, Box, Grid, Typography } from '@material-ui/core';
 import { LocalHotel as LocalHotelIcon } from '@material-ui/icons';
-import { Layout, BlogPostIndex } from 'src/components';
-import { NotFoundPageQuery } from 'src/types';
+import { Layout } from '../components';
 
 const useStyles = makeStyles(theme => ({
   icon: {
@@ -14,72 +13,27 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-const NotFound: React.FC<PageProps<NotFoundPageQuery>> = ({ data }) => {
+const NotFound: React.FC<PageProps> = () => {
   const classes = useStyles();
   const { t } = useI18next();
   return (
-    <Layout icon={data.icon?.svg?.content || ''} iconAlt={data.icon?.title || ''}>
-      <Container maxWidth="md">
-        <Box margin={2} textAlign="center">
-          {/* 見つかりませんでした */}
+    <Layout>
+      <Grid container direction="column" justify="center" alignItems="center">
+        <Box textAlign="center">
           <LocalHotelIcon className={classes.icon} />
           <Typography variant="h4" component="h1" paragraph>
             {t('not-found.title')}
           </Typography>
           <Typography variant="body1">{t('not-found.description')}</Typography>
         </Box>
-        <Box margin={2} textAlign="center">
-          {/* ホームに戻る */}
-          <Button variant="contained" to="/" size="large">
+        <Box marginTop={4} textAlign="center">
+          <Button variant="contained" to="/">
             {t('not-found.back-to-home')}
           </Button>
         </Box>
-        <Box margin={2}>
-          {/* ブログ記事一覧 */}
-          <BlogPostIndex posts={data.posts.group} />
-        </Box>
-      </Container>
+      </Grid>
     </Layout>
   );
 };
 
 export default NotFound;
-
-export const query = graphql`
-  query NotFoundPage($language: String!) {
-    # ブログ記事一覧を取得する
-    posts: allNotionPageBlog(sort: { fields: [tags, createdAt], order: [ASC, DESC] }) {
-      group(field: tags) {
-        edges {
-          node {
-            pageId
-            slug
-            title
-            excerpt
-            tags
-            createdAt
-            pageIcon
-          }
-        }
-      }
-    }
-    # Bicstoneアイコンを取得する
-    # "5qVePilXXNs2WxxIcvndga"は、contentful assetsのアイコンのID
-    icon: contentfulAsset(contentful_id: { eq: "5qVePilXXNs2WxxIcvndga" }) {
-      title
-      svg {
-        content
-      }
-    }
-    # 原稿を取得する
-    locales: allLocale(filter: { language: { eq: $language } }) {
-      edges {
-        node {
-          ns
-          data
-          language
-        }
-      }
-    }
-  }
-`;
---
+++ src/pages/privacy.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { PageProps } from 'gatsby';
+import { Link } from 'gatsby-theme-material-ui';
+import { useI18next } from 'gatsby-plugin-react-i18next';
+import { Container, Typography, Grid, Card, CardContent, Link as ELink } from '@material-ui/core';
+import { Layout } from '../components';
+
+const privacy: React.FC<PageProps> = () => {
+  const { t } = useI18next();
+  return (
+    <Layout cookieAlertShow={false}>
+      <Container maxWidth="md">
+        <Grid item>
+          <Typography component="h2" variant="h6" paragraph>
+            {t('privacy.title')}
+          </Typography>
+          <Card>
+            <CardContent>
+              <Typography paragraph>{t('privacy.description.1')}</Typography>
+              <Typography paragraph>{t('privacy.description.2')}</Typography>
+              <Typography paragraph>
+                <ELink
+                  href="https://policies.google.com/"
+                  rel="external noreferrer noopener nofollow"
+                  target="_blank"
+                >
+                  {t('privacy.description.3')}
+                </ELink>
+              </Typography>
+              <Typography paragraph>{t('privacy.description.4')}</Typography>
+              <Typography paragraph>
+                <Link to="/">{t('privacy.description.5')}</Link>
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Container>
+    </Layout>
+  );
+};
+
+export default privacy;
