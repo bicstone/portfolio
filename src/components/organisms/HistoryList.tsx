75786ea1d92d878ee175548735aa7f32a7b820ee
Takanori Oishi <takanori0014@live.jp>
2021-11-24 00:40:22 +0900

    refactor

--- src/components/organisms/HistoryList.tsx
+++ src/components/organisms/HistoryList.tsx
@@ -23,45 +23,43 @@ export const HistoryList: React.FC<HistoryListProps> = ({ histories }) => {
   const yearToAge = (year: number): number => React.useMemo(() => year - 1996, [year]);
 
   return (
-    <Grid container>
-      <Timeline css={{ padding: 0 }}>
-        {histories.map(
-          ({ node }, index, { length }) =>
-            node.node_locale === language && (
-              <TimelineItem key={node.id} css={{ '&:before': { display: 'none' } }}>
-                <TimelineSeparator
-                  css={theme => ({
-                    flex: `0 0 ${theme.spacing(8)}`,
-                    marginTop: 1,
-                    wordBreak: 'keep-all',
-                    whiteSpace: 'nowrap',
-                  })}
-                >
-                  <Typography variant="body2" color="textSecondary">
-                    {t('histories.date', { date: node.date })}
-                  </Typography>
-                  <Typography variant="body2" color="textSecondary">
-                    {t('histories.age', { age: `${yearToAge(Number(node.date))}` })}
-                  </Typography>
-                  {index < length - 2 && <TimelineConnector />}
-                </TimelineSeparator>
-                <TimelineContent>
-                  <AvatarCard
-                    avatar={
-                      <SvgAvatar name={node?.icon?.name || ''} svg={node?.icon?.svg?.svg || ''} />
-                    }
-                    title={
-                      <Typography component="h2" variant="h6">
-                        {node.name}
-                      </Typography>
-                    }
-                    disableTypography
-                  />
-                </TimelineContent>
-              </TimelineItem>
-            ),
-        )}
-      </Timeline>
-    </Grid>
+    <Timeline css={{ padding: 0 }}>
+      {histories.map(
+        ({ node }, index, { length }) =>
+          node.node_locale === language && (
+            <TimelineItem key={node.id} css={{ '&:before': { display: 'none' } }}>
+              <TimelineSeparator
+                css={theme => ({
+                  flex: `0 0 ${theme.spacing(8)}`,
+                  marginTop: 1,
+                  wordBreak: 'keep-all',
+                  whiteSpace: 'nowrap',
+                })}
+              >
+                <Typography variant="body2" color="textSecondary">
+                  {t('histories.date', { date: node.date })}
+                </Typography>
+                <Typography variant="body2" color="textSecondary">
+                  {t('histories.age', { age: `${yearToAge(Number(node.date))}` })}
+                </Typography>
+                {index < length - 2 && <TimelineConnector />}
+              </TimelineSeparator>
+              <TimelineContent>
+                <AvatarCard
+                  avatar={
+                    <SvgAvatar name={node?.icon?.name || ''} svg={node?.icon?.svg?.svg || ''} />
+                  }
+                  title={
+                    <Typography component="h2" variant="h6">
+                      {node.name}
+                    </Typography>
+                  }
+                  disableTypography
+                />
+              </TimelineContent>
+            </TimelineItem>
+          ),
+      )}
+    </Timeline>
   );
 };
