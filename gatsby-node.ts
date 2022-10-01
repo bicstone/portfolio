a6cc0e362035ceebc90d069732ba56ef9ff1b212
Takanori Oishi <takanori0014@live.jp>
2022-10-01 19:41:59 +0900

    rm templates

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,8 +1,5 @@
 import path from "path";
 
-import { isDefined } from "./src/utils/typeguard";
-
-import type { BlogPostsQuery } from "./src/generated/graphqlTypes";
 import type { GatsbyNode } from "gatsby";
 
 export const onCreateWebpackConfig: GatsbyNode["onCreateWebpackConfig"] = ({
@@ -18,45 +15,7 @@ export const onCreateWebpackConfig: GatsbyNode["onCreateWebpackConfig"] = ({
         "@/hooks": path.resolve("src/hooks"),
         "@/utils": path.resolve("src/utils"),
         "@/layouts": path.resolve("src/layouts"),
-        "@/templates": path.resolve("src/templates"),
       },
     },
-  });
-};
-
-export const createPages: GatsbyNode["createPages"] = async ({
-  graphql,
-  actions,
-}) => {
-  const { createPage } = actions;
-  const result = await graphql<BlogPostsQuery>(`
-    query BlogPosts {
-      allContentfulBlogPost {
-        edges {
-          node {
-            id
-            slug
-          }
-        }
-      }
-    }
-  `);
-
-  if (!isDefined(result.data)) throw new Error();
-
-  result.data.allContentfulBlogPost.edges.forEach((post, index, posts) => {
-    const previous = index === posts.length - 1 ? null : posts[index + 1].node;
-    const next = index === 0 ? null : posts[index - 1].node;
-
-    createPage({
-      path: post.node.slug,
-      component: path.resolve("./src/templates/BlogPost.tsx"),
-      context: {
-        id: post.node.id,
-        slug: post.node.slug,
-        previous,
-        next,
-      },
-    });
   });
 };
