d97736c1670d3307ecb0f9030f4ce5890acfe356
Takanori Oishi <takanori0014@live.jp>
2022-10-06 10:22:58 +0900

    fix

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,6 +1,8 @@
-import { isValid, parseISO } from "date-fns";
-import { format as formatFn } from "date-fns-tz";
+import { isValid } from "date-fns";
+import { format as formatFn, utcToZonedTime } from "date-fns-tz";
 import { ja } from "date-fns/locale";
+
+const timeZone = "Asia/Tokyo";
 
 /**
  * Returns formatted dateTime string from ISO dateTime string.
@@ -8,9 +10,9 @@ import { ja } from "date-fns/locale";
  * see https://date-fns.org/v2.29.3/docs/format
  */
 export const formatDateTime = (value: string, format: string): string => {
-  const parsedDate = parseISO(value);
+  const parsedDate = utcToZonedTime(value, timeZone);
 
   if (!isValid(parsedDate)) return "";
 
-  return formatFn(parsedDate, format, { locale: ja, timeZone: "Asia/Tokyo" });
+  return formatFn(parsedDate, format, { locale: ja, timeZone });
 };
