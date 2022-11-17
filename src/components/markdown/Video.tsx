9ab0b629feb2c9b2195b6286d92b8fd654c2e65a
Takanori Oishi <takanori0014@live.jp>
2022-11-17 22:14:25 +0900

    fix video style

--- src/components/markdown/Video.tsx
+++ src/components/markdown/Video.tsx
@@ -3,9 +3,11 @@ import styled from "@emotion/styled";
 export const Video = styled("video")(({ theme }) => ({
   position: "relative",
   display: "block",
+  width: "100%",
   maxWidth: 600,
   marginLeft: "auto",
   marginRight: "auto",
   borderRadius: theme.spacing(2),
   border: `1px solid ${theme.vars.palette.divider}`,
+  padding: theme.spacing(1),
 }));
