38c9bb63077a2b21123536981cc7f2c3e51c6604
Takanori Oishi <takanori0014@live.jp>
2022-12-03 16:42:57 +0900

    feat: generating OGP image Fix #618

--- src/utils/createOgpImage/index.ts
+++ src/utils/createOgpImage/index.ts
@@ -1,9 +1,4 @@
-import path from "path";
-
-import catchy from "catchy-image";
-
-const fontFamily = "BIZUDPGothic";
-const rootDir = path.resolve(__dirname, "..", "..", "..");
+import { generateImage } from "./generateImage";
 
 export interface CreateOgpImageProps {
   title: string;
@@ -14,53 +9,8 @@ export const createOgpImage = async ({
   title,
   slug,
 }: CreateOgpImageProps): Promise<void> => {
-  const options = {
-    output: {
-      directory: path.resolve(rootDir, "static", "ogp"),
-      fileName: `${slug}.png`,
-    },
-    image: {
-      width: 1200,
-      height: 630,
-      backgroundColor: "#fff",
-      backgroundImage: path.resolve(__dirname, "background.png"),
-    },
-    style: {
-      title: {
-        fontFamily,
-        fontColor: "rgba(0, 0, 0, 0.87)",
-        fontWeight: "bold",
-        fontSize: 60,
-        paddingTop: 100,
-        paddingBottom: 200,
-        paddingLeft: 150,
-        paddingRight: 150,
-      },
-      author: {
-        fontFamily,
-        fontColor: "rgba(0, 0, 0, 0.65)",
-        fontWeight: "400",
-        fontSize: 46,
-      },
-    },
-    meta: {
-      title,
-      author: "Oishi Takanori",
-    },
-    fontFile: [
-      {
-        path: path.resolve(__dirname, fontFamily, "Bold.ttf"),
-        family: fontFamily,
-        weight: "bold",
-      },
-      {
-        path: path.resolve(__dirname, fontFamily, "Regular.ttf"),
-        family: fontFamily,
-        weight: "400",
-      },
-    ],
-    iconFile: path.resolve(rootDir, "static", "android-chrome-512x512.png"),
-  };
-
-  await catchy.generate(options);
+  await generateImage({
+    fileName: `${slug}.png`,
+    title,
+  });
 };
