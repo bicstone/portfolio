a9e3cef8f38c17c69e11175620828032ae80b953
Takanori Oishi <takanori0014@live.jp>
2023-03-05 14:36:55 +0900

    fix

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -169,8 +169,8 @@ const config: GatsbyConfig = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: path.resolve("content", "images"),
-        name: `images`,
+        path: path.resolve("content", "assets"),
+        name: `assets`,
       },
     },
     {
