778de96c8d04c54f61c8e712196d31d672fbe6e7
Takanori Oishi <takanori0014@live.jp>
2023-08-20 16:24:45 +0900

    fix: Twitterをx.comに変更

---
+++ src/components/icons/XIcon.tsx
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+
+import { IconBase } from "./IconBase";
+
+import icon from "@/content/icons/XIcon.svg";
+
+export const XIcon = styled(IconBase)({
+  content: `url(${icon})`,
+});
