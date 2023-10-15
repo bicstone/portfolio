da9dbe200e4004a2dcbf6f7b909c02823495a521
Takanori Oishi <takanori0014@live.jp>
2023-10-15 16:43:08 +0900

    feat: Add facebook

---
+++ src/components/icons/FacebookIcon.tsx
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+
+import { IconBase } from "./IconBase";
+
+import icon from "@/content/icons/FacebookIcon.svg";
+
+export const FacebookIcon = styled(IconBase)({
+  content: `url(${icon})`,
+});
