a880c9024ffdec45a944fad925f80bc805afc14c
Takanori Oishi <takanori0014@live.jp>
2024-02-08 10:09:36 +0900

    fix: Bluesky Icon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,19 @@
 import Container from "@mui/material/Container";
+import { NoSsr } from "@mui/material/NoSsr";
+import Typography from "@mui/material/Typography";
 import { graphql } from "gatsby";
 
 import type { IndexPageQuery } from "@/generated/graphqlTypes";
 import type { PageProps, HeadFC } from "gatsby";
 
+import { InarticleAd } from "@/components/InarticleAd";
 import { Spacer } from "@/components/Spacer";
 import { SITE_METADATA } from "@/constants/SITE_METADATA";
 import { BioCardList } from "@/features/Bio";
 import { TimelineVirtualizedList } from "@/features/Timeline";
 import { TimelineTabList } from "@/features/TimelineTab";
 import { HeadTemplate } from "@/layouts/HeadTemplate";
+import { isDefined } from "@/utils/typeguard";
 
 export const query = graphql`
   query IndexPage {
@@ -49,6 +53,25 @@ const IndexPage = ({ data }: PageProps<IndexPageQuery>): JSX.Element => {
         <TimelineTabList />
         <Spacer y={6} />
         <TimelineVirtualizedList items={timelineItems} />
+        {isDefined(process.env.GATSBY_ADSENSE_PUB_ID) &&
+          isDefined(process.env.GATSBY_ADSENSE_INARTICLE_AD_ID) && (
+            <NoSsr defer>
+              <aside css={(theme) => ({ margin: theme.spacing(4, 0) })}>
+                <Typography
+                  variant="h5"
+                  component="h3"
+                  fontWeight="bold"
+                  paragraph
+                >
+                  広告
+                </Typography>
+                <InarticleAd
+                  pubId={process.env.GATSBY_ADSENSE_PUB_ID}
+                  adId={process.env.GATSBY_ADSENSE_INARTICLE_AD_ID}
+                />
+              </aside>
+            </NoSsr>
+          )}
       </Container>
     </>
   );
