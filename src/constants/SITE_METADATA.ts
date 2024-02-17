53f7e5ae89f63e15dae1ff8f4638f9223c490456
Oishi Takanori <takanori0014@live.jp>
2024-02-17 20:17:16 +0900

    fix: Replace image urls (#978)

--- src/constants/SITE_METADATA.ts
+++ src/constants/SITE_METADATA.ts
@@ -1,16 +1,19 @@
+const siteUrl = process.env.GATSBY_SITE_URL ?? "https://bicstone.me";
+
 export const SITE_METADATA = {
   title: "おおいし (@bicstone)",
   shortTitle: "bicstone",
-  siteUrl: process.env.GATSBY_SITE_URL ?? "https://bicstone.me",
+  siteUrl,
   description:
     "Webエンジニア/機械エンジニア おおいし (@bicstone) のポートフォリオサイトです。",
   author: "bicstone",
-  logoImage: "https://static.bicstone.me/icon_circular.png",
-  ogpImage: "https://static.bicstone.me/og_image.png",
+  logoImage: `${siteUrl}/images/icon_circular.png`,
+  ogpImage: `${siteUrl}/images/og_image.png`,
   firstName: "Takanori",
   lastName: "Oishi",
   defaultLanguage: "ja",
   tileColor: "#81c784",
   maskColor: "#2d1e1e",
-  profileLink: "https://bicstone.notion.site/8d96378d367e4d61833cd01a5357b4bc",
+  profileLink:
+    "https://bicstone.notion.site/Oishi-Takanori-8d96378d367e4d61833cd01a5357b4bc",
 } as const satisfies Record<string, string>;
