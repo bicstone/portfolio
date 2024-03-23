d0488837065e22e34963629a688e358c4e74f98c
Oishi Takanori <takanori0014@live.jp>
2024-03-23 11:12:42 +0900

    feat: Remove in article ads (#1012)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,10 @@
 import Container from "@mui/material/Container";
-import NoSsr from "@mui/material/NoSsr";
 import Typography from "@mui/material/Typography";
 import { graphql } from "gatsby";
 
 import type { IndexPageQuery } from "@/generated/graphqlTypes";
 import type { PageProps, HeadFC } from "gatsby";
 
-import { InarticleAd } from "@/components/InarticleAd";
 import { Spacer } from "@/components/Spacer";
 import { SITE_METADATA } from "@/constants/SITE_METADATA";
 import { BioCardList } from "@/features/Bio";
@@ -15,7 +13,6 @@ import { ArchivedList } from "@/features/TimelineArchived";
 import { FeaturedList } from "@/features/TimelineFeatured";
 import { TimelineTabList } from "@/features/TimelineTab";
 import { HeadTemplate } from "@/layouts/HeadTemplate";
-import { isDefined } from "@/utils/typeguard";
 
 export const query = graphql`
   query IndexPage {
@@ -69,26 +66,6 @@ const IndexPage = ({ data }: PageProps<IndexPageQuery>): JSX.Element => {
         <TimelineTabList />
         <Spacer y={6} />
         <TimelineVirtualizedList items={timelineItems} />
-        <Spacer y={2} />
-        {isDefined(process.env.GATSBY_ADSENSE_PUB_ID) &&
-          isDefined(process.env.GATSBY_ADSENSE_INARTICLE_AD_ID) && (
-            <NoSsr defer>
-              <aside css={(theme) => ({ margin: theme.spacing(4, 0) })}>
-                <Typography
-                  variant="h5"
-                  component="h2"
-                  fontWeight="bold"
-                  paragraph
-                >
-                  広告
-                </Typography>
-                <InarticleAd
-                  pubId={process.env.GATSBY_ADSENSE_PUB_ID}
-                  adId={process.env.GATSBY_ADSENSE_INARTICLE_AD_ID}
-                />
-              </aside>
-            </NoSsr>
-          )}
         <Spacer y={2} />
         <Typography variant="h5" component="h2" fontWeight="bold">
           Archived
--- src/pages/outputs.tsx
+++ src/pages/outputs.tsx
@@ -1,5 +1,4 @@
 import Container from "@mui/material/Container";
-import NoSsr from "@mui/material/NoSsr";
 import Typography from "@mui/material/Typography";
 import { graphql } from "gatsby";
 
@@ -7,7 +6,6 @@ import type { OutputsPageQuery } from "@/generated/graphqlTypes";
 import type { HeadFC, PageProps } from "gatsby";
 
 import { Breadcrumbs } from "@/components/Breadcrumbs";
-import { InarticleAd } from "@/components/InarticleAd";
 import { Spacer } from "@/components/Spacer";
 import { SITE_METADATA } from "@/constants/SITE_METADATA";
 import { TRANSLATION } from "@/constants/TRANSLATION";
@@ -15,7 +13,6 @@ import { TimelineList } from "@/features/Timeline";
 import { ArchivedList } from "@/features/TimelineArchived";
 import { TimelineTabList } from "@/features/TimelineTab";
 import { HeadTemplate } from "@/layouts/HeadTemplate";
-import { isDefined } from "@/utils/typeguard";
 
 export const query = graphql`
   query OutputsPage {
@@ -106,26 +103,6 @@ const OutputsPage = ({ data }: PageProps<OutputsPageQuery>): JSX.Element => {
       <TimelineList groups={outputGroups} />
       <Spacer y={4} />
       <Breadcrumbs title={title} />
-      <Spacer y={2} />
-      {isDefined(process.env.GATSBY_ADSENSE_PUB_ID) &&
-        isDefined(process.env.GATSBY_ADSENSE_INARTICLE_AD_ID) && (
-          <NoSsr defer>
-            <aside css={(theme) => ({ margin: theme.spacing(4, 0) })}>
-              <Typography
-                variant="h5"
-                component="h3"
-                fontWeight="bold"
-                paragraph
-              >
-                広告
-              </Typography>
-              <InarticleAd
-                pubId={process.env.GATSBY_ADSENSE_PUB_ID}
-                adId={process.env.GATSBY_ADSENSE_INARTICLE_AD_ID}
-              />
-            </aside>
-          </NoSsr>
-        )}
       <Spacer y={2} />
       <Typography variant="h5" component="h2" fontWeight="bold">
         Archived
