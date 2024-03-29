25047ccf99152924055abc941bc4be01b2f29ecb
Takanori Oishi <takanori0014@live.jp>
2024-03-29 14:30:14 +0900

    fix

--- src/components/markdown/constants.ts
+++ src/components/markdown/constants.ts
@@ -1,5 +1,5 @@
 export const CONSOLE_FONT_FAMILY =
-  '"UDEV Gothic", HackGen, PlemolJP, Consolas, Courier, monospace' as const;
+  '"UDEV Gothic", HackGen, PlemolJP, Consolas, Courier, monospace';
 
 export const FONT_FAMILY =
-  '"BIZ UDPGothic", "Meiryo UI", Meiryo, -apple-system, BlinkMacSystemFont, sans-serif' as const;
+  '"BIZ UDPGothic", "Meiryo UI", Meiryo, -apple-system, BlinkMacSystemFont, sans-serif';
