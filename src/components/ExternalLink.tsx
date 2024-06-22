929107001e2dc8ed53f932b4bc10a66aec2780db
Takanori Oishi <takanori0014@live.jp>
2024-06-22 22:36:59 +0900

    fix: Remove ExternalLink Icon

--- src/components/ExternalLink.tsx
+++ src/components/ExternalLink.tsx
@@ -1,13 +1,4 @@
-import styled from "@emotion/styled";
-import LaunchIcon from "@mui/icons-material/LaunchRounded";
 import { type ReactNode } from "react";
-
-const StyledLaunchIcon = styled(LaunchIcon)(({ theme }) => ({
-  fontSize: ".8rem",
-  marginLeft: theme.spacing(0.5),
-  color: theme.vars.palette.text.secondary,
-  verticalAlign: "text-bottom",
-}));
 
 export interface ExternalLinkProps {
   children: ReactNode;
@@ -15,12 +6,6 @@ export interface ExternalLinkProps {
 
 export const ExternalLink = ({ children }: ExternalLinkProps): JSX.Element => {
   return (
-    <>
-      {children}
-      <StyledLaunchIcon
-        fontSize="inherit"
-        titleAccess="外部リンクのため、別ウインドウで開きます"
-      />
-    </>
+    <span title="外部リンクのため、別ウインドウで開きます">{children}</span>
   );
 };
