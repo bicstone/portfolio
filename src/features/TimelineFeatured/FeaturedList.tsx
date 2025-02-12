b59dc361a792cd61a4577e4f84c79616159bc807
Takanori Oishi <takanori0014@live.jp>
2025-02-12 09:54:53 +0900

    fix: Update featured list

--- src/features/TimelineFeatured/FeaturedList.tsx
+++ src/features/TimelineFeatured/FeaturedList.tsx
@@ -17,17 +17,6 @@ export const FeaturedList = (): React.JSX.Element => (
   <Container as="section">
     {/* StaticImageを使用するためベタ書き :-/ */}
     <FeaturedCard
-      title="2024年、GraphQLにどう向き合う？導入と活用の実際"
-      url="https://findy.connpass.com/event/328076/"
-    >
-      <StaticImage
-        src="../../../content/featured/j0BfD2l9.jpg"
-        alt="2024年、GraphQLにどう向き合う？導入と活用の実際"
-        width={1200}
-        height={630}
-      />
-    </FeaturedCard>
-    <FeaturedCard
       title="「モノづくり」への憧れがアウトプットの原動力 - Findy Engineer Lab - ファインディエンジニアラボ"
       url="https://findy-code.io/engineer-lab/bicstone_me"
     >
@@ -39,12 +28,12 @@ export const FeaturedList = (): React.JSX.Element => (
       />
     </FeaturedCard>
     <FeaturedCard
-      title="backlog-notify"
-      url="https://github.com/bicstone/backlog-notify"
+      title="モノづくりを愛する機械設計エンジニアが、趣味で始めたWebエンジニアに転身した理由 - Findy Engineer Lab - ファインディエンジニアラボ"
+      url="https://findy-code.io/engineer-lab/turningpoint-bicstone-me"
     >
       <StaticImage
-        src="../../../content/featured/backlog-notify.png"
-        alt="backlog-notify"
+        src="../../../content/featured/20250207144051.png"
+        alt="モノづくりを愛する機械設計エンジニアが、趣味で始めたWebエンジニアに転身した理由 - Findy Engineer Lab - ファインディエンジニアラボ"
         width={1200}
         height={630}
       />
@@ -56,6 +45,17 @@ export const FeaturedList = (): React.JSX.Element => (
       <StaticImage
         src="../../../content/featured/slide_0.jpg"
         alt="アウトプットのモチベーションを維持する4つの掟"
+        width={1200}
+        height={630}
+      />
+    </FeaturedCard>
+    <FeaturedCard
+      title="backlog-notify"
+      url="https://github.com/bicstone/backlog-notify"
+    >
+      <StaticImage
+        src="../../../content/featured/backlog-notify.png"
+        alt="backlog-notify"
         width={1200}
         height={630}
       />
